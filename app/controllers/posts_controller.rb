f36d0f1adbac18ff3e810cb2c248f453d4173521
Cindy <lxx0314@126.com>
2017-08-02 15:14:41 +0800

    implement post_controller edit/update/destroy action, find_post_and_check_permission

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,6 @@
 class PostsController < ApplicationController
   before_action :authenticate_user!
+  before_action :find_post_and_check_permission, only: [:edit, :update, :destroy]
 
   def new
     @group = Group.find(params[:group_id])
@@ -18,7 +19,36 @@ class PostsController < ApplicationController
     end
   end
 
+  def edit
+    @group = Group.find(params[:group_id])
+    @post = Post.find(params[:id])
+  end
+
+  def update
+    @post.group = @group
+    @post.user = current_user
+
+    if @post.update(post_params)
+      redirect_to group_path(@group), notice: "post update successfully"
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @post.destroy
+    redirect_to group_path(@group), alert: "delete post successfully"
+  end
+
   private
+
+  def find_post_and_check_permission
+    @group = Group.find(params[:group_id])
+    @post = Post.find(params[:id])
+    if current_user != @post.user
+      redirect_to group_path(@group), alert: "You have no permission."
+    end
+  end
 
   def post_params
     params.require(:post).permit(:content)
